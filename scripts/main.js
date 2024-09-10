31b867955f0ffe6e15ac1118f8d63a958ba4958f
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-10 22:04:11 +0530

    add swap player logic & draw.io integration

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ const boxes = document.querySelectorAll(".box");
 const newGameBtn = document.querySelector(".btn-reset");
 
 // Game Variables
+const validPlayers = ["X", "O"];
+
 let player;
 let gameGrid;
 
@@ -21,8 +23,23 @@ startGame();
 
 const chances = [];
 
+// Function to swap player
+const swapPlayer = (currPlayer) => {
+  const newPlayer =
+    validPlayer[0] == currPlayer ? validPlayers[1] : validPlayers[0];
+  return newPlayer;
+};
+
 // Function to handle click
 const handleClick = (index) => {
+  // """
+  // Function responsible to
+  // 1. Check if the .box is clickable
+  // 2. Marks the box value to the current player
+  // 3.
+  // 4. Invokes togglePlayer()
+  // 5. Check who won
+  // """
   chances.push(index);
   if (winningChances.includes(chances)) alert("Won!");
 
